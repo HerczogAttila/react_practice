8ac149a75703b55f1060894ebd565710bba6be31
Herczog Attila <aherczog@precognox.com>
2018-02-01 16:40:26 +0100

    first commit

---
+++ src/Person/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './Person.css';
+
+const person = (props) => {
+  const rnd = Math.random();
+
+  if (rnd > 10.7) {
+    throw new Error('Something went wrong');
+  }
+
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} year old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+  );
+};
+
+export default person;
