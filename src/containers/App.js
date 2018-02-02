86a8c8ae7ee7bcafb0d65934d61695d9347faede
Herczog Attila <aherczog@precognox.com>
2018-02-02 12:50:15 +0100

    95. A Different Approach to HOCs

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, {PureComponent} from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -73,7 +74,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit appTitle={this.props.title}
                  persons={this.state.persons}
@@ -81,9 +82,9 @@ class App extends PureComponent {
                  clicked={this.togglePersonsHandler}
         />
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
